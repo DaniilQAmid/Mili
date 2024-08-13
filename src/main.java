93f5c33ce2ede5299f117d0b8a6058e027a993e0
Daniil <tornado9815@mail.ru>
2024-08-13 19:20:55 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 7649;
+        int miles = service.calculate(price);
+        System.out.println(miles);
+    }
+}
